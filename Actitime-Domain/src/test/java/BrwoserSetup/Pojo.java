9ae48a8059062c0289c5ad15c4de98aa59fc145a
PRAVIN BODHARE <PRAVIN BODHARE@DESKTOP-ERKP2PO>
2021-06-22 01:26:44 +0530

    project

---
+++ Actitime-Domain/src/test/java/BrwoserSetup/Pojo.java
@@ -0,0 +1,27 @@
+package BrwoserSetup;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Pojo {
+
+
+	WebDriver driver;
+	
+	public static WebDriver OpenChromeBrowser()
+	{
+		ChromeOptions options=new ChromeOptions();
+		options.addArguments("--disable-notifications");
+		System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\browser\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver(options);
+		return driver;
+	}
+	public static WebDriver OpenFireFoxBrowser()
+	{
+		System.setProperty("webdriver.gecko.driver", "src\\test\\resources\\browser\\geckodriver.exe");
+		WebDriver driver =new FirefoxDriver();
+		return driver;
+	}
+}
